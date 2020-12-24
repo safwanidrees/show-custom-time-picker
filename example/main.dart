e060da59775aa8f842514f52cd3a22646aba1843
Jorge Santana <jorgesanure@gmail.com>
2020-12-24 10:48:34 -0500

    adding example folder

--- example/main.dart
+++ example/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'time_picker_widget.dart';
+import '../lib/time_picker_widget.dart';
 
 void main() {
   runApp(MyApp());
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:time_picker_widget/main.dart';
+import 'file:///C:/Users/Jorge/Documents/dev/flutter/time_picker_widget/example/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
